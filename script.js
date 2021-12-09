d248c0f3d7bcccfc37df2c6bf0959db089c4a342
blupimp21 <94860892+blupimp21@users.noreply.github.com>
2021-12-09 00:07:38 -0700

    2nd commit

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
 // Assignment Code
 // generate password wasnt called here its called
-function generatePassword() {
-  var generateBtn = document.querySelector("#generate");
+function generatePassword(){
   var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   var special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '{', '}', '[', ']', ':', ';', '"', '?', '>', '<', '~'];
   var pC = [];
-
-
-  // Write password to the #password input
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-
-    passwordText.value = password;
-  }
-
 
   //1 prompt the user for the password criteria
   howmany = prompt("Choose a password from 8 to 128 Characters");
@@ -86,9 +75,18 @@ function generatePassword() {
     pC = pC.concat(special);
   }
 
- 
-  //will note you in your console log
-  console.log("Hey you clicked the button");
+
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
 
   // Add event listener to generate button
   generateBtn.addEventListener("click", writePassword);
