66a36900b91948319f672ac466711e49be20cb9f
blupimp21 <94860892+blupimp21@users.noreply.github.com>
2021-12-09 15:38:26 -0700

    My Commit

--- script.js
+++ script.js
@@ -1,93 +1,95 @@
-// Assignment Code
-// generate password wasnt called here its called
-function generatePassword(){
-  var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-  var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  var special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '{', '}', '[', ']', ':', ';', '"', '?', '>', '<', '~'];
-  var pC = [];
-
-  //1 prompt the user for the password criteria
-  howmany = prompt("Choose a password from 8 to 128 Characters");
-  if (howmany < 8 || howmany > 128) {
-    return "Please pick valid number";
-  } else if (isNaN(howmany)) {
-    howmany = prompt("enter valid number");
-  }
-  else {
-    alert("password" + howmany + "characters long");
-  }
-
-  // provides lowercase
-  lower = confirm("Do you want lowercase characters");
-  if (lower) {
-    alert("will contain lowercase letters");
-  }
-  else {
-    alert("No lower case letters");
-  }
-
-  // provides uppercase
-  upper = confirm("Would you like upper case letters");
-  if (upper) {
-    alert("password will contain upper case");
-  }
-  else {
-    alert("password will not contain uppercase letters");
-  }
-
-  // provides numbers
-  numbers = confirm("Would you like numbers?");
-  if (numbers) {
-    alert("password will contain numbers");
-  }
-  else {
-    alert("password will not contain numbers");
-  }
-
-  //provides special characters
-  special = confirm("Would you like special characters?");
-  if (special) {
-    alert("password will contain special characters");
-  }
-  else {
-    alert("password will not contain special characters");
-  }
-
-  if (lower === false && upper === false && numbers === false && special === false){
-    return "Need to type in character";
-  }
-
-  // put selected characters together to make password
-  if (numbers) {
-    pC = pC.concat(numbers);
-  }
-
-  if (upper) {
-    pC = pC.concat(upper);
-  }
-
-  if (lower) {
-    pC = pC.concat(lower);
-  }
-
-  if (special) {
-    pC = pC.concat(special);
-  }
-
-
+//Assignment code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//special characters
+const specChar1 = "!@#$%^&*()_+:><~";
+const generateButton = document.getElementById('generateBtn')
+generateButton.addEventListener('click', writePassword)
 
-  passwordText.value = password;
+//#password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
 
 }
 
+//promtps client what choices are available
+function generatePassword() {
+    var pwL = prompt("Pick a number between 8 and 128 for your password");
+     if (!pwL || pwL < 8 || pwL > 128) {
+         window.alert ("Invalid Length");
+     }
 
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+    var num = confirm("Would you like numbers in your pw?");
+    var lowCase = confirm("Would you like lowercase in your pw?");
+    var upCase = confirm("Would you like uppercase in your pw?");
+    var specChar = confirm("Would you like special characters in your pw?");
+
+
+//min counter
+var minCount = 0;
+
+//empty for var
+var minNum = "";
+var minLowCase = "";
+var minUpCase = "";
+var minSpecChar = "";
+
+//functions generator
+var functionArray = {
+    getNum: function () {
+        return String.fromCharCode(Math.floor(Math.random() * 48 + 57));
+    },
+    getLowCase: function () {
+        return String.fromCharCode(Math.floor(Math.random() * 98 + 122));
+    },
+    getUpCase: function () {
+        return String.fromCharCode(Math.floor(Math.random() * 65 + 90));
+    },
+    getSpecChar: function () {
+        return String.fromCharCode(Math.floor(Math.random() * 33 + 46));
+    }
+};
+
+// client selection checked
+if (num === true){
+    minNum = functionArray.getNum();
+    minCount++;
+}
+
+if (lowCase === true){
+    minLowCase = functionArray.getLowCase();
+    minCount++;
+}
+
+if (upCase === true){
+    minUpCase = functionArray.getUpCase();
+    minCount++;
+}
+
+if (specChar === true){
+    minSpecChar = functionArray.getSpecChar();
+    minCount++;
+}
+
+//loop for string
+var rPG = "";
+
+// loop cont for retrieving random
+for (let i = 0; i < (parseInt(pwL) - minCount); i++){
+    var rNP = Math.floor(Math.random() * 4);
+
+    rPG += rNP;
+}
+
+//input char in pw
+rPG += minNum;
+rPG += minLowCase;
+rPG += minUpCase;
+rPG += minSpecChar;
+
+return rPG;
+
 }
